e54a48ec7d763dd94ad12adf2da78d8a9954bffd
Yang Lin <todoubaba@gmail.com>
2017-02-18 16:45:40 +0800

    Refactor report service (#23)

--- tm-card-web/grails-app/init/cn/edu/bnuz/bell/tm/card/web/Application.groovy
+++ tm-card-web/grails-app/init/cn/edu/bnuz/bell/tm/card/web/Application.groovy
@@ -2,25 +2,22 @@ package cn.edu.bnuz.bell.tm.card.web
 
 import cn.edu.bnuz.bell.config.ExternalConfigLoader
 import cn.edu.bnuz.bell.menu.module.EnableModuleMenu
+import cn.edu.bnuz.bell.report.EnableReportClient
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.EnvironmentAware
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Scope
-import org.springframework.context.annotation.ScopedProxyMode
 import org.springframework.core.env.Environment
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
-import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
 @EnableResourceServer
 @EnableEurekaClient
 @EnableGlobalMethodSecurity(prePostEnabled=true)
 @EnableModuleMenu
+@EnableReportClient
 class Application extends GrailsAutoConfiguration implements EnvironmentAware {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
@@ -29,12 +26,5 @@ class Application extends GrailsAutoConfiguration implements EnvironmentAware {
     @Override
     void setEnvironment(Environment environment) {
         ExternalConfigLoader.load(environment)
-    }
-
-    @LoadBalanced
-    @Bean()
-    @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
-    RestTemplate restTemplate() {
-        return new RestTemplate()
     }
 }
