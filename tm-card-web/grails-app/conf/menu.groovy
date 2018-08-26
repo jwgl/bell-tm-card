a543ef4a720dab5efdd8652835c04c73fe7ba62f
Yang Lin <todoubaba@gmail.com>
2018-08-26 23:30:45 +0800

    Upgrade to grails 3.3.8 (#46)

--- tm-card-web/grails-app/conf/menu.groovy
+++
@@ -1,7 +0,0 @@
-menuGroup 'main', {
-    affair 40, {
-        cardReissueForm     40, 'PERM_CARD_REISSUE_WRITE',   '/web/card/students/${userId}/reissues'
-        cardReissueApproval 41, 'PERM_CARD_REISSUE_APPROVE', '/web/card/approvers/${userId}/reissues'
-        cardReissueOrder    42, 'PERM_CARD_REISSUE_APPROVE', '/web/card/reissueOrders'
-    }
-}
